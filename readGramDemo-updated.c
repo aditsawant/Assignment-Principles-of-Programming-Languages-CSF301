96222ff920691b2d1cf8178c73ef22226bb14fe6
Amey Pate <54042697+mr-ameyzing@users.noreply.github.com>
2020-10-22 12:15:56 +0530

    Update readGramDemo-updated.c

--- readGramDemo-updated.c
+++ readGramDemo-updated.c
@@ -22,6 +22,21 @@ typedef struct llnode{
 } llnode;
 
 
+typedef struct tokenStream{
+	char token_name[30];
+	char lexeme[30];
+	int line_num;
+	struct tokenStream* next;
+} tokenStream;
+
+typedef struct ntree {
+    char *word;
+    
+    struct node *child;   // point to children of this node
+    struct node *next;    // point to next node at same level
+} ntree;
+
+
 void readGrammar(char* fname, llnode* grammar){
 
 	FILE* fptr = fopen(fname, "r");
@@ -93,23 +108,92 @@ void readGrammar(char* fname, llnode* grammar){
 	fclose(fptr);
 }
 
+char* identifyToken(char* c){
+	char * s = (char*)malloc(sizeof(char)*30);
+	if(s == NULL) printf("malloc unsuccessful\n");
+	if(strcmp(c,"program") == 0) return s = "program";
+	else if(strcmp(c,"(") == 0) return s = "open_p";
+	else if(strcmp(c,")") == 0) return s = "close_p";
+	else if(strcmp(c,"{") == 0) return s = "open_curl";
+	else if(strcmp(c,"}") == 0) return s = "close_curl";
+	else if(strcmp(c,"declare") == 0) return s = "declare";
+	else if(strcmp(c,"list") == 0) return s = "list";
+	else if(strcmp(c,"of") == 0) return s = "of";
+	else if(strcmp(c,"variables") == 0) return s = "variables";
+	else if(strcmp(c,":") == 0) return s = "colon";
+	else if(strcmp(c,";") == 0) return s = "semicol";
+	else if(strcmp(c,"integer") == 0) return s = "integer";
+	else if(strcmp(c,"real") == 0) return s = "real";
+	else if(strcmp(c,"boolean") == 0) return s = "boolean";
+	else if(strcmp(c,"array") == 0) return s = "array";
+	else if(strcmp(c,"jagged") == 0) return s = "jagged";
+	else if(strcmp(c,"[") == 0) return s = "open_sq";
+	else if(strcmp(c,"]") == 0) return s = "close_sq";
+	else if(strcmp(c,"..") == 0) return s = "rangeop";
+	else if(strcmp(c,"R1") == 0) return s = "R1";
+	else if(strcmp(c,"size") == 0) return s = "size";
+	else if(strcmp(c,"values") == 0) return s = "values";
+	else if(strcmp(c,"epsilon") == 0) return s = "epsilon";
+	else if(strcmp(c,"+") == 0) return s = "plus";
+	else if(strcmp(c,"-") == 0) return s = "minus";
+	else if(strcmp(c,"=") == 0) return s = "assignop";
+	else if(strcmp(c,"*") == 0) return s = "mul";
+	else if(strcmp(c,"/") == 0) return s = "div";
+	else if(strcmp(c,"|||") == 0) return s = "or";
+	else if(strcmp(c,"&&&") == 0) return s = "and";
+	else if(c[0] > 47 && c[0] <= 57) return s = "num";
+	else return s = "id";
+}
+
+tokenStream* tokeniseSourcecode(char* fname, tokenStream *head){
+	FILE* fptr = fopen(fname, "r");
+	int line_num = 1;
+	char buffer[200];
+	head = (tokenStream*)malloc(sizeof(tokenStream));
+	tokenStream* s;
+	s = head;
+	while (fgets(buffer, sizeof(buffer), fptr) != NULL) {
+        
+		char *sym_read;
+		int i;
+		sym_read = strtok(buffer, " \n");
+		for (i = 0; sym_read != NULL; i++){
+			tokenStream* temp = (tokenStream*)malloc(sizeof(tokenStream));
+			temp->line_num = line_num;
+			strcpy(temp->lexeme,sym_read);
+			strcpy(temp->token_name, identifyToken(sym_read));
+			temp->next = NULL;
+			s->next = temp;
+			s = temp;
+			sym_read = strtok(NULL, " \n");
+		}
+		line_num++; 
+	} 
+	fclose(fptr);
+	return head->next;
+}
+
+
 int main(){
-	//printf("1");
+	/*
 	llnode* G = (llnode*) malloc(sizeof(llnode)*80);
-    //printf("%lu \n",sizeof(*G)); 
-	//printf("2");
 	readGrammar("grammar.txt", G);
-	//printf("\n3");
+	
 	llnode temp = G[0];
-	//printf("4");
     printf("\n");
 	while(temp.next != NULL){
-		//printf("5");
 		if(temp.sym.is_terminal) printf("%s ", temp.sym.t);
 		else printf("%s ", temp.sym.nt);
 
 		temp = *temp.next;
 	}
-	//printf("6");
+	*/
+	tokenStream* head;
+	head = tokeniseSourcecode("sourcecode.txt", head);
+
+	while(head != NULL){
+		printf("%s\n", head->token_name);
+		head = head->next;
+	}
 	return 0;
 }
